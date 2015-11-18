47837d0823f457ba43bbf5b44c36275ed86c368b
leedu708 <djl265@cornell.edu>
2015-11-18 12:33:06 -0500

    sendgrid setup

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,17 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :address           => 'smtp.sendgrid.net'
+  :port              => '587'
+  :authentication    => :plain,
+  :user_name         => ENV['SENDGRID_USERNAME'],
+  :SENDGRID_PASSWORD => ENV['SENDGRID_PASSWORD'],
+  :domain            => 'heroku.com'
+  :enable_starttls_auto => true
+}
+config.action_mailer.delivery_method ||= :smtp
+config.action_mailer.default_url_options = {
+  :host => 'https://guarded-brushlands-9981.herokuapp.com/'
+}
