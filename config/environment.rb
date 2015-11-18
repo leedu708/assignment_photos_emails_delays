b86b90ee00fc04292517219752f90694ab104b5b
leedu708 <djl265@cornell.edu>
2015-11-18 12:35:25 -0500

    commit to heroku for testing

--- config/environment.rb
+++ config/environment.rb
@@ -14,6 +14,3 @@ ActionMailer::Base.smtp_settings = {
   :enable_starttls_auto => true
 }
 config.action_mailer.delivery_method ||= :smtp
-config.action_mailer.default_url_options = {
-  :host => 'https://guarded-brushlands-9981.herokuapp.com/'
-}
